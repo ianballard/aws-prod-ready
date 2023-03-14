f459049aab76ebb825869d280098c4e79f345157
Ian Ballard <iballard1995@gmail.com>
2023-03-13 21:56:53 -0400

    add auth

--- api/auth/auth_controller.py
+++ api/auth/auth_controller.py
@@ -1,15 +1,36 @@
-import json
-from core_lib.utils.log_util import log_info
-from api_lib.auth.authorization import authorize
+from core_lib.services.auth_service import sign_up, initiate_user_password_auth
+from api_lib.auth.authorization import authorize, Authorization, UserGroup, get_principle
+from core_lib.utils.lambda_util import lambda_handler
+from api_lib.request.api_request import ApiRequest, api
+from api_lib.response.api_response import ApiResponse
 
 
-def authenticate(event, context):
-    log_info("log test")
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": "hello world",
-            }
-        ),
-    }
+@lambda_handler()
+@api()
+def signup(api_request: ApiRequest):
+
+    request_body = api_request.body
+    email = request_body.get('email')
+    password = request_body.get('password')
+    first_name = request_body.get('first_name')
+    last_name = request_body.get('last_name')
+    signup_response = sign_up(
+        email=email,
+        password=password,
+        first_name=first_name,
+        last_name=last_name
+    )
+    return ApiResponse(request_headers=api_request.headers, status_code=201, response_body=signup_response).format()
+
+@lambda_handler()
+@api()
+def authenticate(api_request: ApiRequest):
+    request_body = api_request.body
+    username = request_body.get('username')
+    password = request_body.get('password')
+    authentication_response = initiate_user_password_auth(
+        username=username,
+        password=password
+    )
+    return ApiResponse(request_headers=api_request.headers, status_code=200, response_body=authentication_response).format()
+
--- api/user/user_controller.py
+++ api/user/user_controller.py
@@ -1,16 +1,13 @@
-import json
-from core_lib.utils.log_util import log_info
-from api_lib.auth.authorization import authorize
+from api_lib.auth.authorization import authorize, Authorization, UserGroup, get_principle
+from core_lib.utils.lambda_util import lambda_handler
+from api_lib.request.api_request import ApiRequest, api
+from api_lib.response.api_response import ApiResponse
 
+@lambda_handler()
+@api()
+@authorize(Authorization(user_group=UserGroup.User))
+def get(api_request: ApiRequest):
 
-@authorize('user')
-def get(event, context):
-    log_info("log test")
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": "hello world",
-            }
-        ),
-    }
+    print(get_principle())
+
+    return ApiResponse(api_request.headers, status_code=200, response_body={"foo": "bar"}).format()
