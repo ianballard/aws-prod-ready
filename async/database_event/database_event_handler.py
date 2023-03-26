2b6099ec71281f29a1140cc923ab2d15859616a4
Ian Ballard <iballard1995@gmail.com>
2023-03-26 18:01:34 -0400

    data pipeline, s3 logging, replication, security

--- async/database_event/database_event_handler.py
+++ async/database_event/database_event_handler.py
@@ -1,7 +1,25 @@
-from user.user_lifecycle_events import handle_user_event
-from core_lib.utils.lambda_util import lambda_handler
-from core_lib.utils.log_util import log_unexpected_exception, log_warning
+import json
+
+import boto3
+
 from core_lib.services.database.impl.dynamodb_service import dynamodb_json_to_json
+from core_lib.services.parameter.parameter_service import get_parameter_value, ParameterName
+from core_lib.utils.lambda_util import lambda_handler
+from core_lib.utils.log_util import log_unexpected_exception, log_warning, log_info
+from user.user_lifecycle_events import handle_user_event
+
+firehose = boto3.client('firehose')
+FIREHOSE_DELIVERY_STREAM = None
+
+
+def get_firehose_delivery_stream():
+    global FIREHOSE_DELIVERY_STREAM
+
+    if FIREHOSE_DELIVERY_STREAM is None:
+        FIREHOSE_DELIVERY_STREAM = get_parameter_value(parameter_name=ParameterName.FIREHOSE_DELIVERY_STREAM)
+
+    return FIREHOSE_DELIVERY_STREAM
+
 
 
 @lambda_handler()
@@ -30,8 +48,28 @@ def handle_db_event(event):
                 continue
 
             entity_response = entity_type_handler(event_name, old_image, new_image)
+
+            put_firehose_record(event_name, old_image, new_image)
+
+
         except Exception as e:
             log_unexpected_exception(e)
 
 
 ENTITY_TYPES = {"user": handle_user_event}
+
+
+def put_firehose_record(event_name, old_image, new_image):
+
+    record = new_image if event_name in ["INSERT", "MODIFY"] else old_image
+
+    if event_name == 'REMOVE':
+        record['is_hard_deleted'] = True
+
+    delivery_stream_name = get_firehose_delivery_stream()
+    log_info(f'putting record to firehose delivery stream: {delivery_stream_name} record: {record}')
+
+    firehose.put_record(
+        DeliveryStreamName=delivery_stream_name,
+        Record={"Data": json.dumps(record)},
+    )
