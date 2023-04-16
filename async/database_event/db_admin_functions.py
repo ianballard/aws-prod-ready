88df5124679dad03020194b5ade8c6ab3bb610fd
Ian Ballard <iballard1995@gmail.com>
2023-04-16 13:37:37 -0400

    fix linting

--- async/database_event/db_admin_functions.py
+++ async/database_event/db_admin_functions.py
@@ -1,20 +1,11 @@
-import json
 import os
 
 import boto3
 
 from core_lib.services.cloud_function.impl import lambda_service
-from core_lib.services.database import database_service
-from core_lib.services.database.impl.dynamodb_service import dynamodb_json_to_json
-from core_lib.services.parameter.parameter_service import (
-    get_parameter_value,
-    ParameterName,
-)
 from core_lib.utils.cloudformation_custom_resource_util import send_cfn_response
 from core_lib.utils.lambda_util import lambda_handler
-from core_lib.utils.log_util import log_unexpected_exception, log_warning, log_info
 from core_lib.utils.thread_util import safe_get_thread_attribute
-from user.user_lifecycle_events import handle_user_event
 
 firehose = boto3.client("firehose")
 
