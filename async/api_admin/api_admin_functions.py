fdbd507e60bb1a661abe80f87c950261ad8fbe51
Ian Ballard <iballard1995@gmail.com>
2023-04-19 11:50:24 -0400

    fix api docs

--- async/api_admin/api_admin_functions.py
+++ async/api_admin/api_admin_functions.py
@@ -1,3 +1,6 @@
+import json
+from datetime import datetime
+
 from core_lib.utils.cloudformation_custom_resource_util import send_cfn_response
 from core_lib.utils.lambda_util import lambda_handler
 from core_lib.utils.thread_util import safe_get_thread_attribute
@@ -6,24 +9,44 @@ from core_lib.services.file_storage import file_storage_service
 import boto3
 
 api_gateway_client = boto3.client("apigateway")
+cloudfront_client = boto3.client('cloudfront')
 
 
 @lambda_handler()
 def manage_api_exports(event):
     api_id = event.get("ResourceProperties", {}).get("ApiId")
     bucket_name = event.get("ResourceProperties", {}).get("BucketName")
+    cloud_front_dist = event.get("ResourceProperties", {}).get("CloudFrontDistribution")
+    api_url = event.get("ResourceProperties", {}).get("ApiUrl")
     if event.get("RequestType") in ["Create", "Update"] and api_id and bucket_name:
-        file_key = get_api_file_key(api_id=api_id)
-        if not file_key:
-            log_warning(f"api not found: {api_id}")
 
-        api_definition = get_export_content(api_id=api_id)
-        if not api_definition:
-            log_warning(f"unable to export: {api_id}")
+        try:
 
-        file_storage_service.upload(
-            bucket_name=bucket_name, key=file_key, content=api_definition
-        )
+            file_key = get_api_file_key(api_id=api_id)
+            if not file_key:
+                log_warning(f"api not found: {api_id}")
+
+            api_definition = get_export_content(api_id=api_id).decode('utf-8')
+
+            if not api_definition:
+                log_warning(f"unable to export: {api_id}")
+
+            api_definition_json = json.loads(api_definition)
+
+            api_definition_json['servers'] = [
+                {
+                    "url": api_url
+                }
+            ]
+
+            file_storage_service.upload(
+                bucket_name=bucket_name, key=file_key, content=json.dumps(api_definition_json)
+            )
+
+            create_invalidation(distribution_id=cloud_front_dist)
+
+        except Exception as e:
+            log_unexpected_exception(e)
 
     send_cfn_response(
         event,
@@ -58,5 +81,32 @@ def get_api_file_key(api_id: str):
 
         return file_key
 
+    except Exception as e:
+        log_unexpected_exception(e)
+
+
+def create_invalidation(distribution_id: str):
+    try:
+        return cloudfront_client.create_invalidation(
+            DistributionId=distribution_id,
+            InvalidationBatch={
+                'Paths': {
+                    'Quantity': 1,
+                    'Items': [
+                        '/*',
+                    ]
+                },
+                'CallerReference': str(datetime.utcnow())
+            }
+        )
+    except Exception as e:
+        log_unexpected_exception(e)
+
+
+def get_distribution(distribution_id: str):
+    try:
+        return cloudfront_client.get_distribution(
+            Id=distribution_id
+        ).get('Distribution')
     except Exception as e:
         log_unexpected_exception(e)
