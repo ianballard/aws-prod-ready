831ee4aef619470c5fb167ac9e001f3bbfe2df90
Ian Ballard <iballard1995@gmail.com>
2023-04-23 10:45:50 -0400

    add basic metrics and alarms

--- async/log_management/log_group_management/process_new_log_group.py
+++ async/log_management/log_group_management/process_new_log_group.py
@@ -7,22 +7,24 @@ from core_lib.utils.log_util import log_warning
 
 @lambda_handler()
 def handle_new_log_group_created(event):
-    log_group_name = event.get('detail', {}).get('requestParameters', {}).get('logGroupName')
+    log_group_name = (
+        event.get("detail", {}).get("requestParameters", {}).get("logGroupName")
+    )
 
     if not log_group_name:
-        log_warning('no log group name provided')
+        log_warning("no log group name provided")
         return
 
     put_subscription_filter(
         log_group=log_group_name,
         destination_arn=os.getenv("APP_ERROR_LOGGER_FUNCTION_ARN"),
         filter_name="error_log_filter",
-        filter_pattern="APP_ERROR_LOG"
+        filter_pattern="APP_ERROR_LOG",
     )
 
     put_subscription_filter(
         log_group=log_group_name,
         destination_arn=os.getenv("APP_ACCESS_LOGGER_FUNCTION_ARN"),
         filter_name="access_log_filter",
-        filter_pattern="APP_ACCESS_LOG"
+        filter_pattern="APP_ACCESS_LOG",
     )
