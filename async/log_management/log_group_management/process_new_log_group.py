07680d92b0ae265b4b5cbef4537ce3d055085f13
Ian Ballard <iballard1995@gmail.com>
2023-04-24 12:09:26 -0400

    make sure new log group is not the same as the destination for the logs

--- async/log_management/log_group_management/process_new_log_group.py
+++ async/log_management/log_group_management/process_new_log_group.py
@@ -2,29 +2,51 @@ import os
 
 from core_lib.services.logs.impl.cloudwatch_logs_service import put_subscription_filter
 from core_lib.utils.lambda_util import lambda_handler
-from core_lib.utils.log_util import log_warning
+from core_lib.utils.log_util import log_warning, log_info
 
 
 @lambda_handler()
 def handle_new_log_group_created(event):
-    log_group_name = (
+    new_log_group_name = (
         event.get("detail", {}).get("requestParameters", {}).get("logGroupName")
     )
 
-    if not log_group_name:
+    if not new_log_group_name:
         log_warning("no log group name provided")
         return
 
-    put_subscription_filter(
-        log_group=log_group_name,
-        destination_arn=os.getenv("APP_ERROR_LOGGER_FUNCTION_ARN"),
-        filter_name="error_log_filter",
-        filter_pattern="APP_ERROR_LOG",
-    )
+    error_log_destination_arn = os.getenv("APP_ERROR_LOGGER_FUNCTION_ARN")
+    if (
+        f"/aws/lambda/{get_lambda_function_name(error_log_destination_arn)}"
+        != new_log_group_name
+    ):
+        put_subscription_filter(
+            log_group=new_log_group_name,
+            destination_arn=error_log_destination_arn,
+            filter_name="error_log_filter",
+            filter_pattern="APP_ERROR_LOG",
+        )
+    else:
+        log_info(
+            "new log group is the error log target destination. skipping subscription filter creation."
+        )
 
-    put_subscription_filter(
-        log_group=log_group_name,
-        destination_arn=os.getenv("APP_ACCESS_LOGGER_FUNCTION_ARN"),
-        filter_name="access_log_filter",
-        filter_pattern="APP_ACCESS_LOG",
-    )
+    access_log_destination_arn = os.getenv("APP_ACCESS_LOGGER_FUNCTION_ARN")
+    if (
+        f"/aws/lambda/{get_lambda_function_name(access_log_destination_arn)}"
+        != new_log_group_name
+    ):
+        put_subscription_filter(
+            log_group=new_log_group_name,
+            destination_arn=access_log_destination_arn,
+            filter_name="access_log_filter",
+            filter_pattern="APP_ACCESS_LOG",
+        )
+    else:
+        log_info(
+            "new log group is the access log target destination. skipping subscription filter creation."
+        )
+
+
+def get_lambda_function_name(arn):
+    return arn.split(":")[-1]
